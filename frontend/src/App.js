8d51dae5f80d1531bdabd0f82fff5ac2c7297a01
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-02-07 21:58:05 +0700

    Frontend User Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
   return (
@@ -15,10 +18,13 @@ function App() {
       <main className="py-3">
         <Container>
           <Routes>
-            <Route exact path="/" element={<HomeScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
-            <Route exact path="/cart" element={<CartScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
           </Routes>
         </Container>
       </main>

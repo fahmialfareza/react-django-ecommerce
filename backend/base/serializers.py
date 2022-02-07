2ad8261955c64dddb1ffb595b0b1e10881024b2f
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-02-07 16:35:17 +0700

    Backend User Authentication

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,43 @@
+from pyexpat import model
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import AccessToken
 from .models import Product
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+    def get__id(self, object):
+        return object.id
+
+    def get_isAdmin(self, object):
+        return object.is_staff
+
+    def get_name(self, object):
+        name = object.first_name
+        if name == '':
+            name = object.email
+
+        return name
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, object):
+        token = AccessToken.for_user(object)
+        return str(token)
 
 
 class ProductSerializer(serializers.ModelSerializer):
